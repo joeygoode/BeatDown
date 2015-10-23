30ab0d12f08e9f6b82eab71637c7b08bc13b502d
Joey Goode <joey@clypd.com>
2015-10-23 00:17:47 -0400

    Add a spawner and a simple AI to run at the player
    closes #2

---
+++ TopDownTest/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class EnemySpawner : MonoBehaviour {
+	public int MaxSpawns;
+	public float SpawnInterval;
+	public GameObject Spawn;
+
+	private float spawnCountdown;
+	public List<GameObject> spawns;
+
+	// Use this for initialization
+	void Start () {
+		spawns = new List<GameObject> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		spawns = spawns.Where (s => s != null).ToList ();
+
+		if (spawnCountdown > 0) {
+			spawnCountdown -= Time.deltaTime;
+		} else {
+			if (spawns.Count < MaxSpawns) {
+				spawns.Add ((GameObject) Instantiate(Spawn, transform.position, transform.rotation));
+			}
+			spawnCountdown = SpawnInterval;
+		}
+	}
+}

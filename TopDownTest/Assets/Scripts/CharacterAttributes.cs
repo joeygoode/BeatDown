de0fa36a8943f90dab6a3cba995a984c2b08f4fa
Tyler Kaminsky <tylerkaminsky@gmail.com>
2015-10-28 16:30:28 -0400

    got it now.  Adding character attributes

---
+++ TopDownTest/Assets/Scripts/CharacterAttributes.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAttributes : MonoBehaviour {
+
+	public int characterHealth = 100;
+	public int attackDamage = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(CharacterDead()) {
+
+			// End Game
+			UnityEditor.EditorApplication.isPlaying = false;
+		}
+
+
+	}
+
+	// Is the character's health gone
+	bool CharacterDead() {
+
+		return (characterHealth <= 0);
+	}
+}

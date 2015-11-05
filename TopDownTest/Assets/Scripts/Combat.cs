3934d11e6212dcb12829d085de168d03e2b6742f
Tyler Kaminsky <tylerkaminsky@gmail.com>
2015-11-05 01:37:11 -0500

    Models, animation, no skeletons, added files this time

---
+++ TopDownTest/Assets/Scripts/Combat.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Combat : MonoBehaviour {
+
+
+	public GameObject targetEnemy;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey (KeyCode.Space)) {
+
+			// GOTTA GO BACK TO FIX AN ANIMATION ISSUE
+			ClickToMove.attack = true;
+			// When animating,
+
+			if(targetEnemy != null) {
+
+				transform.LookAt(targetEnemy.transform.position);
+				//targetEnemy.GetComponent<SimpleAI>.getHit(10);
+			}
+
+		}
+
+//		if(!Animation.IsPlaying(attack.name)) {
+//			ClickToMove.attack = false;
+//		}
+
+	}
+}
